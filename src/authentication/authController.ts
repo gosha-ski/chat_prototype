dc6d8ff9ad01ba6eb222d6082ecd7d401a72f14f
gosha <yaren042@mail.ru>
2022-08-05 17:49:39 +0500

    added authentication controller

---
+++ src/authentication/authController.ts
@@ -0,0 +1,68 @@
+import {Router} from "express"
+import {UserModel} from "../users/userModel"
+import * as uniqid from "uniqid"
+
+export class AuthController{
+	router = Router()
+	path = "/auth"
+
+	constructor(){
+		this.initRoutes()
+	}
+
+	private initRoutes(){
+		this.router.post(`${this.path}/login`, this.login)
+		this.router.post(`${this.path}/register`, this.register)
+	}
+
+	private login = async(request, response)=>{
+		try{
+			let body = request.body
+			let user = (await UserModel.findAll({
+				where:{
+					email: body.email
+				}
+			}))[0]
+
+			if(user){
+				if(body.password == user.get("password")){
+					response.send("success sign in")
+				}else{
+					response.send("invalid passsword")
+				}
+			}else{
+				response.send("User with this email not found")
+			}
+
+		}catch(error){
+			console.log(error)
+		}
+	}
+
+	private register = async(request, response)=>{
+		try{
+			let body = request.body
+			let user = (await UserModel.findAll({
+				where:{
+					email: body.email
+				}
+			}))[0]
+
+			if(user){
+				response.send("user with this email already registed")
+			}else{
+				await UserModel.create({
+					id: uniqid(),
+					email: body.email,
+					name: body.email,
+					password: body.password
+				})
+				response.send("success sign up")
+			}
+
+		}catch(error){
+			console.log(error)
+		}
+	}
+
+}
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 import {UserController} from "./users/userController"
+import {AuthController} from "./authentication/authController"
 import {App} from "./app"
 
 let app = new App([
-	new UserController()
+	new UserController(),
+	new AuthController()
 ])
